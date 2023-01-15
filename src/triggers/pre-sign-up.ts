809dafa833f8db491f6606c9ca4edfa70d578d1c
Shairbekov Bakyt <cyberstalker0726@gmail.com>
2023-01-15 23:11:43 +0600

    feature/added_post-signUp_trigger/save_to_dynamo

--- src/triggers/pre-sign-up.ts
+++ src/triggers/pre-sign-up.ts
@@ -1,33 +1,9 @@
 import { PreSignUpTriggerEvent, PreSignUpTriggerHandler } from 'aws-lambda';
 
-//import { DynamoDB } from '../common/dynamo/Dynamo';
-//import { TableKeys, UserAttributes } from '../common/dynamo/schema';
-//import { buildUserKey } from '../common/dynamo/buildKey';
-
-//const dynamoDB = new DynamoDB();
-
 export const preSignUp: PreSignUpTriggerHandler = async (
 	event: PreSignUpTriggerEvent
 ) => {
 	console.log('Received EVENT', JSON.stringify(event, null, 2));
-	/*
-	const phoneNumber = event.request.userAttributes.phone_number;
-
-	const userKey = buildUserKey(phoneNumber);
-
-	const params = {
-		[TableKeys.PK]: userKey,
-		[TableKeys.SK]: userKey,
-		[UserAttributes.FIRST_NAME]: '',
-		[UserAttributes.LAST_NAME]: '',
-		[UserAttributes.BALANCE]: 100,
-		[UserAttributes.PHONE_NUMBER]: phoneNumber,
-		[UserAttributes.CREATED_AT]: Date.now().toString(),
-		[UserAttributes.UPDATED_AT]: '',
-		[UserAttributes.EMAIL]: '',
-	};
-  await dynamoDB.putItem(process.env.dynamo_table as string, params);
-  */
 
 	event.response.autoConfirmUser = true;
 	event.response.autoVerifyPhone = true;
