0d66b47cda0d41814dc233bffebd7242ba2c5bce
AidarIonov <aidarionov@gmail.com>
2023-01-17 18:12:18 +0600

    added user in respose sign-in verify

--- src/endpoints/sign-in-verify/sign-in-verify.ts
+++ src/endpoints/sign-in-verify/sign-in-verify.ts
@@ -1,8 +1,11 @@
 import { AuthService } from '../../authService';
 import { APIGatewayEvent, Context, APIGatewayProxyCallback } from 'aws-lambda';
+import { buildUserKey } from '../../common/dynamo/buildKey';
+import { DynamoDB } from '../../common/dynamo/Dynamo';
+import { TableKeys } from '../../common/dynamo/schema';
 
 const authService = new AuthService();
-
+const dynamoDB = new DynamoDB();
 export const signInVerify = async (
 	event: APIGatewayEvent,
 	context: Context,
@@ -10,12 +13,22 @@ export const signInVerify = async (
 ) => {
 	try {
 		const { phoneNumber, passCode, session } = JSON.parse(event.body as string);
+		const tableName = process.env.dynamo_table as string;
+		const userKey = buildUserKey(phoneNumber);
 
 		const res = await authService.verifySignIn(passCode, phoneNumber, session);
 
+		const userOutput = await dynamoDB.getItem(tableName, {
+			[TableKeys.PK]: userKey,
+			[TableKeys.SK]: userKey,
+		});
+
 		callback(null, {
 			statusCode: 201,
-			body: JSON.stringify(res),
+			body: JSON.stringify({
+				...res.AuthenticationResult,
+				user: userOutput.Item,
+			}),
 		});
 	} catch (error: unknown) {
 		if (error instanceof Error) {
