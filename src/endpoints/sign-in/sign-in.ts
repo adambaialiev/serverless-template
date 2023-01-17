ab527a4f3ee4a64aa5aaa8c3a0398c9dfcfe87fe
AidarIonov <aidarionov@gmail.com>
2023-01-17 17:16:57 +0600

    removed sign-up endpoint

--- src/endpoints/sign-in/sign-in.ts
+++ src/endpoints/sign-in/sign-in.ts
@@ -1,7 +1,11 @@
 import { AuthService } from '../../authService';
 import { APIGatewayEvent, Context, APIGatewayProxyCallback } from 'aws-lambda';
+import { DynamoDB } from '../../common/dynamo/Dynamo';
+import { buildUserKey } from '../../common/dynamo/buildKey';
+import { TableKeys } from '../../common/dynamo/schema';
 
 const authService = new AuthService();
+const dynamoDB = new DynamoDB();
 
 export const signIn = async (
 	event: APIGatewayEvent,
@@ -10,6 +14,17 @@ export const signIn = async (
 ) => {
 	try {
 		const { phoneNumber } = JSON.parse(event.body as string);
+		const tableName = process.env.dynamo_table as string;
+		const userKey = buildUserKey(phoneNumber);
+
+		const userOutput = await dynamoDB.getItem(tableName, {
+			[TableKeys.PK]: userKey,
+			[TableKeys.SK]: userKey,
+		});
+
+		if (!userOutput.Item) {
+			await authService.signUp(phoneNumber);
+		}
 
 		const res = await authService.signIn(phoneNumber);
 
