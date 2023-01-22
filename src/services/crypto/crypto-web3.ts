7054dc75ef98c84934c90725dd503046dbeb85df
AidarIonov <aidarionov@gmail.com>
2023-01-22 16:12:39 +0600

    added get user endpoint

--- src/services/crypto/crypto-web3.ts
+++ src/services/crypto/crypto-web3.ts
@@ -1,7 +1,11 @@
 import Web3 from 'web3';
 import { contractAbi } from './constants';
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://goerli.infura.io/v3/49dfcdb7a3254aaab0ff651e6d0ed870'));
+const web3 = new Web3(
+	new Web3.providers.HttpProvider(
+		'https://goerli.infura.io/v3/49dfcdb7a3254aaab0ff651e6d0ed870'
+	)
+);
 
 class CryptoWeb3Service {
 	createCryptoWallet() {
@@ -16,18 +20,28 @@ class CryptoWeb3Service {
 		const ownerAddress = '0xC7E966B2b80738458ddF304D586058E900a4C25b';
 		const nonce = await web3.eth.getTransactionCount(ownerAddress, 'latest');
 		const transaction = {
-			'to': '0xb5A59D7AbAD4f5aa0c95FD799C17985ed8b3bb5e',
-			'value': web3.utils.toWei('.000001', 'ether'),
-			'gas': '128028',
-			'gasPrice': web3.utils.toWei('0.00000002', 'ether'),
-			'nonce': nonce,
+			to: '0xb5A59D7AbAD4f5aa0c95FD799C17985ed8b3bb5e',
+			value: web3.utils.toWei('.000001', 'ether'),
+			gas: '128028',
+			gasPrice: web3.utils.toWei('0.00000002', 'ether'),
+			nonce: nonce,
 		};
-		const signedTx = await web3.eth.accounts.signTransaction(transaction, '0x997ac31ca6c895d405287e2a2eed95de7e1093e373ba93844f6c95744f821333');
+		const signedTx = await web3.eth.accounts.signTransaction(
+			transaction,
+			'0x997ac31ca6c895d405287e2a2eed95de7e1093e373ba93844f6c95744f821333'
+		);
 		web3.eth.sendSignedTransaction(signedTx.rawTransaction, (error, hash) => {
 			if (!error) {
-				console.log('🎉 The hash of your transaction is: ', hash, '\n Check Alchemy\'s Mempool to view the status of your transaction!');
+				console.log(
+					'🎉 The hash of your transaction is: ',
+					hash,
+					'\n Check Alchemy\'s Mempool to view the status of your transaction!'
+				);
 			} else {
-				console.log('❗Something went wrong while submitting your transaction:', error);
+				console.log(
+					'❗Something went wrong while submitting your transaction:',
+					error
+				);
 			}
 		});
 	}
