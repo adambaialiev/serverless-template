bf0e7eeefafd81d818e3d6e90b1cb4aa057a7bab
Shairbekov Bakyt <cyberstalker0726@gmail.com>
2023-01-23 13:32:00 +0600

    feature/added_endpoint_transactionsByAdmin_for_web

---
+++ src/endpoints/getTransactionByAdmin/getTransactionByAdmin.ts
@@ -0,0 +1,24 @@
+import AWS from 'aws-sdk';
+import { sendResponse } from '@/utils/makeResponse';
+//import { TableKeys } from '@/common/dynamo/schema';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.dynamo_table as string;
+
+export const getTransactions = async () => {
+	try {
+		const params = {
+			TableName: tableName,
+			FilterExpression: 'begins_with(SK, :sk)',
+			ExpressionAttributeValues: {
+				':sk': 'TRANSACTION#',
+			},
+		};
+
+		const transactions = await dynamoDB.scan(params).promise();
+		return sendResponse(200, { transactions });
+	} catch (error) {
+		const message = error.message ? error.message : 'Internal server error';
+		return sendResponse(500, { message });
+	}
+};
