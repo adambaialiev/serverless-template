16f2787746f42fa721dd6da930cb6b615037840b
adambaialiev <adam.baialiev@gmail.com>
2023-02-18 14:52:10 +0600

    cleanup

--- src/endpoints/store/getStoreValuesForAdmin.ts
+++ src/endpoints/store/getStoreValuesForAdmin.ts
@@ -1,14 +1,7 @@
-import CryptoEthersService from '@/services/crypto/cryptoEthers';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 const handler: APIGatewayProxyHandler = async (event, context, callback) => {
 	try {
-		const cryptoService = new CryptoEthersService();
-		await cryptoService.makePolygonMaticTransaction(
-			'0xf751022586ac7eb183acc858a9c834225f0e7e3d20a14f1386d8672aab696128',
-			'0x4851671c8501b0B27907512fb4E2d9321D4B9836',
-			'0.01'
-		);
 		callback(null, {
 			statusCode: 201,
 			body: JSON.stringify(true),
