ed7963e00696cfa40f0aeb38b5e0727ddc913dfa
adambaialiev <adam.baialiev@gmail.com>
2023-02-18 17:33:13 +0600

    add init endpoint

--- src/endpoints/sign-in-verify/sign-in-verify.ts
+++ src/endpoints/sign-in-verify/sign-in-verify.ts
@@ -3,7 +3,6 @@ import { DynamoMainTable } from '@/common/dynamo/DynamoMainTable';
 import { TableKeys, UserItem } from '@/common/dynamo/schema';
 import { AuthService } from '@/services/auth/auth';
 import { CryptoService } from '@/services/crypto/crypto';
-import MasterWallet from '@/services/masterWallet/masterWallet';
 import UserService from '@/services/user/user';
 import { sendResponse } from '@/utils/makeResponse';
 import { APIGatewayEvent } from 'aws-lambda';
@@ -37,10 +36,7 @@ export const signInVerify = async (event: APIGatewayEvent) => {
 			}
 
 			const cryptoService = new CryptoService();
-			const masterWallet = new MasterWallet();
 			const userService = new UserService();
-
-			await masterWallet.createMasterWalletIfNeeded();
 
 			const user = await userService.getUser(phoneNumber);
 
