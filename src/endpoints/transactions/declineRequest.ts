010e8d1dfd6e15d0f733fa5a4292125a57a92ad7
AidarIonov <aidarionov@gmail.com>
2023-02-21 14:53:56 +0600

    added push notification on transac request decline

--- src/endpoints/transactions/declineRequest.ts
+++ src/endpoints/transactions/declineRequest.ts
@@ -5,8 +5,12 @@ import {
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { sendResponse } from '@/utils/makeResponse';
 import { TransactionService } from '@/services/transaction/transactionService';
+import { PushNotifications } from '@/services/pushNotifications/pushNotification';
+import UserService from '@/services/user/user';
 
 const transactionsService = new TransactionService();
+const pushNotificationService = new PushNotifications();
+const userService = new UserService();
 
 const handler: APIGatewayProxyHandler = async (event: CustomAPIGateway) => {
 	try {
@@ -15,11 +19,20 @@ const handler: APIGatewayProxyHandler = async (event: CustomAPIGateway) => {
 
 		const source = event.user;
 
+		const userTarget = await userService.getSlug(phoneNumber);
+
 		const res = await transactionsService.declineRequest(
 			source.phoneNumber,
 			phoneNumber,
 			id
 		);
+
+		if (userTarget.pushToken) {
+			await pushNotificationService.send(
+				userTarget.pushToken,
+				`User ${source.phoneNumber} declined your request`
+			);
+		}
 
 		return sendResponse(200, res);
 	} catch (error: unknown) {
