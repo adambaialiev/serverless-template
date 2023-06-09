909430b7086e6351e541f3e83d9990364d91c3f0
adambaialiev <adam.baialiev@gmail.com>
2023-06-09 16:04:45 +0600

    fix

--- src/endpoints/monitoring/createMonitoringInfo.ts
+++ src/endpoints/monitoring/createMonitoringInfo.ts
@@ -1,36 +1,9 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { sendResponse } from '@/utils/makeResponse';
-import MonitoringService from '@/services/monitoring/monitoringService';
-import { SlackNotifications } from '@/utils/slackNotifications';
-import { getName } from 'country-list';
 
-const monitoring = new MonitoringService();
-
-const handler: APIGatewayProxyHandler = async (event) => {
+const handler: APIGatewayProxyHandler = async () => {
 	try {
-		console.log({ event });
-		const { eventName, coin, metaData } = JSON.parse(event.body);
-		const sourceCountryCode = event.headers[
-			'CloudFront-Viewer-Country'
-		] as string;
-		const country = getName(sourceCountryCode);
-
-		const postData = {
-			eventName,
-			coin,
-			metaData: { ...metaData, country },
-		};
-
-		const response = await monitoring.create(postData);
-
-		await SlackNotifications.sendMessage(
-			'monitoring',
-			'SLACK_MONITORING_URL',
-			sourceCountryCode,
-			JSON.stringify(postData, null, '\t')
-		);
-
-		return sendResponse(201, response);
+		return sendResponse(201, true);
 	} catch (error: unknown) {
 		console.log(error);
 		if (error instanceof Error) {
